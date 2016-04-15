564366d1b96ba11267fbc8f796159c9435d48a7f
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-04-15 11:31:02 +0200

    Add policy example for ha-sync-batch-size
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- conf/set_rabbitmq_policy.sh
+++ conf/set_rabbitmq_policy.sh
@@ -1,7 +1,7 @@
 # This script is called by rabbitmq-server-ha.ocf during RabbitMQ
 # cluster start up. It is a convenient place to set your cluster
 # policy here, for example:
-# ${OCF_RESKEY_ctl} set_policy ha-all "." '{"ha-mode":"all", "ha-sync-mode":"automatic"}' --apply-to all --priority 0
+# ${OCF_RESKEY_ctl} set_policy ha-all "." '{"ha-mode":"all", "ha-sync-mode":"automatic", "ha-sync-batch-size":10000}'
 
 # Enable ha-policy with the replica factor of 5 for jepsen queues
 ocf_log info "${LH} Setting HA policy for all queues"
