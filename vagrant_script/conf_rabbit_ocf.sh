7dd9a042980017d5a406c95a7c24014602f24183
Bogdan Dobrelya <bogdando@mail.ru>
2021-07-01 16:30:56 +0200

    Fix OCF RA type ref
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- vagrant_script/conf_rabbit_ocf.sh
+++ vagrant_script/conf_rabbit_ocf.sh
@@ -4,10 +4,10 @@ mkdir -p "/usr/lib/ocf/resource.d/${OCF_RA_PROVIDER}"
 if [ "${UPLOAD_METHOD}" = "copy" ] ; then
   [ "${OCF_RA_PATH}" ] || exit 1
   echo "Get the OCF RA from ${OCF_RA_PATH}"
-  cp -f "${OCF_RA_PATH}" "/usr/lib/ocf/resource.d/${OCF_RA_PROVIDER}/${OCF_RA_PROVIDER_TYPE}"
+  cp -f "${OCF_RA_PATH}" "/usr/lib/ocf/resource.d/${OCF_RA_PROVIDER}/${OCF_RA_TYPE}"
 else
   echo "Download the OCF RA from the stable branch"
-  wget "${OCF_RA_PATH}"  -O "/usr/lib/ocf/resource.d/${OCF_RA_PROVIDER}/${OCF_RA_PROVIDER_TYPE}"
+  wget "${OCF_RA_PATH}"  -O "/usr/lib/ocf/resource.d/${OCF_RA_PROVIDER}/${OCF_RA_TYPE}"
 fi
-chmod +x "/usr/lib/ocf/resource.d/${OCF_RA_PROVIDER}/${OCF_RA_PROVIDER_TYPE}"
+chmod +x "/usr/lib/ocf/resource.d/${OCF_RA_PROVIDER}/${OCF_RA_TYPE}"
 exit 0
